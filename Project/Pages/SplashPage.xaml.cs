a5a326cb7e0ee9b6fd04c9ac1272746fe5b39954
Khangeldi Shakhmukhanbetov <khanekshakh@gmail.com>
2025-04-30 12:47:57 +0500

    Добавил анимацию при запуске приложении

--- Project/Pages/SplashPage.xaml.cs
+++ Project/Pages/SplashPage.xaml.cs
@@ -1,22 +1,47 @@
-using Project.Services;
+﻿using Project.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Project.Pages;
 
 public partial class SplashPage : ContentPage
 {
-	private readonly AuthService _authService;
-	public SplashPage(AuthService authService)
-	{
-		InitializeComponent();
-		Animate();
+    private readonly AuthService _authService;
+    private bool _isAnimating = true;
+
+    public SplashPage(AuthService authService)
+    {
+        InitializeComponent();
         _authService = authService;
+        StartLoadingAnimation();
+        NavigateToNextPage();
     }
 
-	private async void Animate()
-	{
-        await Task.Delay(1000);
+    private async void StartLoadingAnimation()
+    {
+        string baseText = "Загрузка";
+        int dotCount = 0;
 
-        Application.Current.MainPage = new AppShell(_authService); 
+        while (_isAnimating)
+        {
+            dotCount = (dotCount + 1) % 4;
+            LoadingLabel.Text = baseText + new string('.', dotCount);
+            await Task.Delay(500);
+        }
     }
+
+    private async void NavigateToNextPage()
+    {
+        await Task.Delay(3000);
+        _isAnimating = false;
+
+        await Task.WhenAll(
+            LogoImage.ScaleTo(2, 500, Easing.CubicOut),
+            LogoImage.FadeTo(0, 500),
+            this.FadeTo(0, 500)
+        );
+
+        Application.Current.MainPage = new AppShell(_authService);
+    }
+
+
 }
